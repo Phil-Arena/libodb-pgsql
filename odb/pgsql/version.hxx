d232725b99ddcafe05a84a3a378166163b9efb47
Boris Kolpackov <boris@codesynthesis.com>
2013-12-06 09:56:25 +0200

    Bump version to 2.3.1

--- odb/pgsql/version.hxx
+++ odb/pgsql/version.hxx
@@ -36,8 +36,8 @@
 // libodb-pgsql version: odb interface version plus the bugfix
 // version.
 //
-#define LIBODB_PGSQL_VERSION     2030000
-#define LIBODB_PGSQL_VERSION_STR "2.3.0"
+#define LIBODB_PGSQL_VERSION     2030100
+#define LIBODB_PGSQL_VERSION_STR "2.3.1"
 
 #include <odb/post.hxx>
 
